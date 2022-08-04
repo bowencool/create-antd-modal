c3587bd0da4ccfd264bbd80b47fa9e50aa537990
Bowen <root@bowen.cool>
2022-08-04 11:21:23 +0800

    perf: build

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -1,6 +1,17 @@
 export default {
   // more father 4 config: https://github.com/umijs/father-next/blob/master/docs/config.md
-  esm: {},
-  cjs: {},
-  umd: {},
+  esm: {
+    ignores: ['**/*/demos/'],
+  },
+  cjs: {
+    ignores: ['**/*/demos/'],
+  },
+  umd: {
+    externals: {
+      antd: 'antd',
+      react: 'React',
+      'react-dom': 'ReactDOM',
+    },
+    // ignores: ['demos/'],
+  },
 };
