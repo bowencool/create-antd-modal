e1da2c1d2311a85bb51161306a3ecb384a206b0a
Bowen <root@bowen.cool>
2023-11-20 18:40:48 +0800

    feat: useModalCreation

--- src/createModal/demos/context.tsx
+++ src/createModal/demos/context.tsx
@@ -1,22 +1,24 @@
 import React from 'react';
 import { Button } from 'antd';
-import { createModal } from 'create-antd-modal';
+import { useModalCreation } from 'create-antd-modal';
 import RootContainer from './RootContainer';
 
 const Demo: React.FC = () => {
+  const [contextHolder, createModal] = useModalCreation();
   return (
-    <Button
-      onClick={() => {
-        createModal({
-          title: 'Some title',
-          content: 'You can see that the i18n and theme configuration works now',
-          /* For this prop, we strongly recommend using `createFunctionWithDefaultProps` (just like the next demo) */
-          modalRender: (modal) => <RootContainer>{modal}</RootContainer>,
-        });
-      }}
-    >
-      Context Providing
-    </Button>
+    <RootContainer>
+      <Button
+        onClick={() => {
+          createModal({
+            title: 'Some title',
+            content: 'You can see that the i18n and theme configuration works now',
+          });
+        }}
+      >
+        Context Providing
+      </Button>
+      {contextHolder}
+    </RootContainer>
   );
 };
 
